eca603d281e511eaf3df7d0062c8ca42d5615d79
srydev01 <srydev01@gmail.com>
2022-04-15 12:38:00 +0700

    agian

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,7 @@ import styles from './home.module.css'
 export default function Home({ user }) {
   return (
     <div className={styles.container}>
-      <Button href={user ? '/init_consulting_service' : '/login'} variant="outline-primary">บริการให้คำปรึกษาเบื้องต้น</Button>
+      <Button href={user ? '/initconsulting' : '/login'} variant="outline-primary">บริการให้คำปรึกษาเบื้องต้น</Button>
     </div>
   )
 }
