de2cb84cc24c857efde8379a8d826bc7a8be62b8
srydev01@gmail.com <srydev01@gmail.com>
2022-05-04 22:24:57 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 require("dotenv").config()
-const withReactSvg = require('next-react-svg')
 const path = require('path')
 
 module.exports = {
-  env: {
-    REDIS: process.env.REDIS_URL,
-  },
   include: path.resolve(__dirname, 'assets/svg'),
-  webpack(config, options) {
+  webpack(config) {
     return config
   }
 }
